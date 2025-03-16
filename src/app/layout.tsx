02424c0f1b1b76024515ceb171097b52c68c040c
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-03-16 15:52:26 -0300

    feat: add sheet component from shadcui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import HeaderComponent from "./Components/Header-Component/headerComponent";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -18,8 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}</body>
+    <html lang="pt-BR">
+      <body
+        className={`${poppins.className} 
+      antialiased`}
+      >
+        <HeaderComponent />
+        {children}
+      </body>
     </html>
   );
 }
