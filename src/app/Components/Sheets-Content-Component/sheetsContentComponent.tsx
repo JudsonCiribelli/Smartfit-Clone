c9ca08afd83f9ef1c6c26f91ac96240c6060953a
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-03-16 17:22:10 -0300

    feat: add sheets content component

---
+++ src/app/Components/Sheets-Content-Component/sheetsContentComponent.tsx
@@ -0,0 +1,21 @@
+import { SheetContent, SheetHeader } from "@/components/ui/sheet";
+import Image from "next/image";
+import SidebarContent from "../Sidebar-Content/sidebarContent";
+
+const SheetsContent = () => {
+  return (
+    <SheetContent>
+      <SheetHeader>
+        <Image
+          src="https://assets3.smartfit.com.br/assets/new-home-v4-assets/logo-dark.svg"
+          alt="logo"
+          width={124}
+          height={124}
+        />
+      </SheetHeader>
+      <SidebarContent />
+    </SheetContent>
+  );
+};
+
+export default SheetsContent;
