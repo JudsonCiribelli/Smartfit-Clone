bd7445f6c24214c30c224da757b3389923c676de
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-03-19 17:39:13 -0300

    feat: add card plans component

---
+++ src/app/Components/Card-Plans-Component/cardPlans.tsx
@@ -0,0 +1,54 @@
+import { Plan } from "@prisma/client";
+import { CircleCheck } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { FormatCurrency } from "@/helpers/format-currency";
+
+interface PlansCardProps {
+  plans: Plan;
+}
+
+const PlansCard = ({ plans }: PlansCardProps) => {
+  console.log(plans.additionalInfo);
+  return (
+    <Card className="h-[600px] w-[350px] gap-4 border-none">
+      {/* Div com as informações */}
+      <CardHeader>
+        <CardTitle>{plans.name}</CardTitle>
+        <CardDescription className="text-white">
+          {plans.description}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <h2>A PARTIR DE</h2>
+        <h1 className="text-2xl font-semibold text-white">
+          {FormatCurrency(plans.price)}
+        </h1>
+        <p>no primeiro mes, depois R$ 299,90</p>
+        <p className="text-sm text-slate-400">
+          {plans.duration} meses de fidelidade
+        </p>
+        <Button className="my-6 w-full">Contratar agora</Button>
+        <ul className="mt-4 space-y-2 text-white">
+          {plans.additionalInfo.map((info, index) => (
+            <li key={index} className="text-sm">
+              <div className="flex gap-4">
+                <CircleCheck size={14} />
+                {info}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PlansCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import BannersComponent from "./Components/Banners-Component/bannersComponent";
+import { getPlans } from "@/data/get-plans";
 
-export default function Home() {
+import BannersComponent from "./Components/Banners-Component/bannersComponent";
+import PlansCard from "./Components/Card-Plans-Component/cardPlans";
+
+const Home = async () => {
+  const plans = await getPlans();
   return (
     <main className="block overflow-hidden">
       <BannersComponent />
+      <section className="m-8 flex w-full flex-col items-center gap-2">
+        {/* Title */}
+        <div className="p-2 text-center">
+          <h1 className="text-3xl font-semibold text-black">
+            Venha treinar na maior rede de academias da América Latina
+          </h1>
+          <p className="text-lg font-semibold">
+            Os melhores equipamentos e infraestrutura com mensalidades
+            acessíveis.
+          </p>
+        </div>
+        {/* Planos (Componetizados) */}
+        <div className="flex items-center justify-center gap-4 p-8">
+          {plans.map((plan) => (
+            <PlansCard plans={plan} key={plan.id} />
+          ))}
+        </div>
+      </section>
     </main>
   );
-}
+};
+export default Home;
