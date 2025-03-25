185669a6abefe98f7df7e02bc9b984a0d9da59cf
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-03-24 22:47:32 -0300

    feat: add initial setup on modalities page

---
+++ src/app/modalidades-treino/[modalidades]/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+const TrainingModalities = () => {
+  return (
+    <>
+      <header className="h-20 w-full bg-black"></header>
+      <div>
+        {/* texto */}
+        <div></div>
+        {/* Imagem */}
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default TrainingModalities;
