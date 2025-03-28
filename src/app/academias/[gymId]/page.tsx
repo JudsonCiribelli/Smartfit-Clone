4316096e7dbc1151f5cd8b87cfe7f7c266c059e3
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-03-28 01:15:27 -0300

    feat: add initial setup on gym details page

--- src/app/academias/[gymId]/page.tsx
+++ src/app/academias/[gymId]/page.tsx
@@ -1,6 +1,16 @@
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
+import { CardContent } from "@/app/Components/ui/card";
 import { db } from "@/lib/prisma";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../../Components/ui/carousel";
 
 interface GymDetailsProps {
   params: Promise<{ gymId: string }>;
@@ -18,10 +28,40 @@ const GymDetails = async ({ params }: GymDetailsProps) => {
     return notFound();
   }
   return (
-    <div>
-      <p>{gym.name}</p>
-      <p>{gym.id}</p>
-    </div>
+    <main>
+      <section className="mx-auto my-16 flex w-[100%] items-center justify-center gap-20">
+        <div>
+          <div className="-mb-10 ml-8 flex flex-col">
+            <h3 className="text-lg text-black">Academia</h3>
+            <h1 className="text-2xl font-bold text-black">{gym.name}</h1>
+          </div>
+          <Carousel className="w-full max-w-[720px]">
+            <CarouselContent>
+              {gym.images.map((imageUrl, index) => (
+                <CarouselItem key={index}>
+                  <div className="">
+                    <CardContent className="flex aspect-square items-center justify-center">
+                      <Image
+                        src={imageUrl}
+                        alt={`Image ${index + 1} of ${gym.name}`}
+                        width={580}
+                        height={500}
+                        className="w-full rounded-lg object-cover"
+                      />
+                    </CardContent>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+        <div>
+          <h1>{gym.phone}</h1>
+        </div>
+      </section>
+    </main>
   );
 };
 
