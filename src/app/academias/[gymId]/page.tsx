794a72207e3b70340bd347a7f5b96e842bc38054
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-03-28 22:43:12 -0300

    feat: finish section with gym info and images from the respective gym

--- src/app/academias/[gymId]/page.tsx
+++ src/app/academias/[gymId]/page.tsx
@@ -1,6 +1,8 @@
+import { Clock10, Globe, MapPinned } from "lucide-react";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
+import { Button } from "@/app/Components/ui/button";
 import { CardContent } from "@/app/Components/ui/card";
 import { db } from "@/lib/prisma";
 
@@ -57,8 +59,81 @@ const GymDetails = async ({ params }: GymDetailsProps) => {
             <CarouselNext />
           </Carousel>
         </div>
-        <div>
-          <h1>{gym.phone}</h1>
+        <div className="flex flex-col gap-4">
+          {/* Primeiro Card */}
+          <div className="flex h-[200px] w-[400px] gap-4 rounded-xl border p-6">
+            {/* Icon */}
+            <div>
+              <Clock10 size={36} />
+            </div>
+            {/* Horarios */}
+            <div className="flex flex-col gap-1">
+              <h1 className="text-lg text-black">Horário de funcionamento</h1>
+              <div className="mt-2 flex items-center justify-between">
+                <p className="text-sm text-black">Seg a Sex</p>
+                <p className="text-sm text-black">5h - 23h</p>
+              </div>
+              <div className="flex items-center justify-between">
+                <p className="text-sm text-black">Sáb</p>
+                <p className="text-sm text-black">8h - 17h</p>
+              </div>
+              <div className="flex items-center justify-between">
+                <p className="text-sm text-black">Dom/Feriados</p>
+                <p className="text-sm text-black">8h - 14h</p>
+              </div>
+              <Button
+                variant="outline"
+                className="mt-2 border-none text-black underline hover:bg-white"
+              >
+                Ver ocupação da unidade
+              </Button>
+            </div>
+          </div>
+          {/* Segundo Card */}
+          <div className="flex h-[150px] w-[400px] gap-4 rounded-xl border p-6">
+            {/* Icon */}
+            <div>
+              <MapPinned size={36} />
+            </div>
+            {/* Texto */}
+            <div>
+              <div className="flex flex-col gap-1">
+                <h1 className="text-lg text-black">Endereço</h1>
+                <p className="text-xs text-black">{gym.address}</p>
+              </div>
+              <Button
+                variant="outline"
+                className="mt-2 w-[250px] border-none text-black underline hover:bg-white"
+              >
+                Ver rota
+              </Button>
+            </div>
+          </div>
+          {/* Terceito Card */}
+          <div className="flex h-[180px] w-[400px] gap-4 rounded-xl border p-6">
+            {/* Icon */}
+            <div>
+              <Globe size={36} />
+            </div>
+            {/* Texto */}
+            <div>
+              <div className="flex flex-col gap-1">
+                <h1 className="text-lg text-black">
+                  Faça um tour virtual na academia!
+                </h1>
+                <p className="text-xs text-black">
+                  Com o tour virtual você consegue conhecer a academia antes de
+                  contratar o plano!
+                </p>
+              </div>
+              <Button
+                variant="outline"
+                className="mt-2 w-[250px] border-none text-black underline hover:bg-white"
+              >
+                Ver rota
+              </Button>
+            </div>
+          </div>
         </div>
       </section>
     </main>
