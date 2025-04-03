a5077ba991858d8b1356c17ad026e73aa38cb8d5
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-04-02 22:53:29 -0300

    feat: add initial setup on the modalities training details page

--- src/app/modalidades-treino/[workoutId]/page.tsx
+++ src/app/modalidades-treino/[workoutId]/page.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
+import { Button } from "@/app/Components/ui/button";
 import { db } from "@/lib/prisma";
 
 interface WorkoutPageDetailsProps {
@@ -18,9 +20,33 @@ const WorkoutPageDetails = async ({ params }: WorkoutPageDetailsProps) => {
   }
   console.log({ workout });
   return (
-    <div>
-      <h1>{workout.name}</h1>
-    </div>
+    <>
+      <section className="h-14 w-[100%] bg-black"></section>
+      <section className="w-[full] bg-foreground">
+        <div className="flex items-center justify-center gap-10 p-8">
+          {/* Description */}
+          <div className="flex w-[30%] flex-col gap-4">
+            <h1 className="text-3xl font-semibold text-black">
+              {workout.name}
+            </h1>
+            <p className="text-lg text-black">{workout.trainingMethod}</p>
+            <Button className="w-[250px] p-4" variant="destructive">
+              Escolha sua academia
+            </Button>
+          </div>
+          {/* Image */}
+          <div>
+            <Image
+              src={workout.imageUrl}
+              alt="Image-training"
+              width={400}
+              height={400}
+              className="rounded-xl"
+            />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
